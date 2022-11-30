5904007458a846f064b40ad87e4ff6d138dbc3dc
ibuki <bigfrog0102@gmail.com>
2022-12-01 02:06:10 +0800

    remove jsdom code

--- app.js
+++ app.js
@@ -5,35 +5,6 @@ const port = 3000
 // require express-handlebars here
 const exphbs = require('express-handlebars')
 
-const jsdom = require('jsdom')
-const { JSDOM } = jsdom
-const dom = new JSDOM(`<html lang='en'>
-<head>
-  <meta charset='UTF-8' />
-  <meta http-equiv='X-UA-Compatible' content='IE=edge' />
-  <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-  <title>Simple Blog</title>
-  <link rel='stylesheet' href='/stylesheets/bootstrap.css' />
-</head>
-<body>
-  <header class='d-flex justify-content-between'>
-    <div class='homepage-container'>
-      <a href='/' class='btn btn-secondary homepage'>回到首頁</a>
-    </div>
-
-    <div class='nav-container d-flex'>
-      <a href='/about' class='btn btn-secondary navbar about'>About</a>
-      <a
-        href='/portfolio'
-        class='btn btn-secondary navbar portfolio'
-      >Portfolio</a>
-      <a href='/contact' class='btn btn-secondary navbar contact'>Contact</a>
-    </div>
-  </header>
-</body>
-</html>`)
-const document = dom.window.document
-
 const nav = document.querySelector('.navbar')
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -52,18 +23,6 @@ app.get('/portfolio', (req, res) => {
 })
 app.get('/contact', (req, res) => {
   res.render('index', { content: 'Contact' })
-})
-
-nav.addEventListener('click', (event) => {
-  event.preventDefault()
-  console.log('hi')
-  const activeLink = document.querySelector('.active')
-  if (activeLink) {
-    activeLink.classList.remove('active')
-  }
-  if (event.target.matches('.nav')) {
-    event.target.classList.add('active')
-  }
 })
 
 // start and listen on the Express server
